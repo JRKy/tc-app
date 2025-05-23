4fb728c86e33f570223c89361de62a6c96761bd6
Josh Kennedy <josh.kennedy@gmail.com>
2025-05-23 11:21:11 -0600

    Delete date-fns-tz.umd.js

--- date-fns-tz.umd.js
+++
@@ -1,9 +0,0 @@
-
-// Placeholder for date-fns-tz UMD bundle
-// In actual deployment, replace this with the real content from:
-// https://cdn.jsdelivr.net/npm/date-fns-tz@1.3.7/dist/date-fns-tz.umd.min.js
-window.dateFnsTz = {
-  utcToZonedTime: function (date, tz) {
-    return new Date(date.toLocaleString('en-US', { timeZone: tz }));
-  }
-};
