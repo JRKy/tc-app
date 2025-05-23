5e747fd307d0e46abe6d65e07f5aae6730eff54e
Josh Kennedy <josh.kennedy@gmail.com>
2025-05-22 23:00:04 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -241,6 +241,7 @@ function addZone() {
     alert("Invalid time zone. Please select from the suggestions.");
     return;
   }
+  }
   if (!selectedZones.includes(zone)) {
     selectedZones.push(zone);
     input.value = "";
